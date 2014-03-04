3b920211fe06167dbf33399d84ac561863cea868
Krishna <krishnamoniz@live.nl>
2014-03-04 11:24:47 +0800

    camera activity

--- test/src/com/kris/test/CameraActivity.java
+++ test/src/com/kris/test/CameraActivity.java
@@ -1,54 +1,47 @@
 package com.kris.test;
 
+import com.kris.test.camera.CameraPreview;
+
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 
 public class CameraActivity extends Activity {
 	
 	public static final int RESULT_FINISH 	= 11;
 	
 	protected Toast toastMsg;
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
 	
 	@Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_button);
-        ((TextView) findViewById(R.id.textView1)).setText("Main Activity");
+        setContentView(R.layout.activity_camera);
+        ((TextView) findViewById(R.id.button_capture)).setText("New text");
+        
+        if(checkCameraHardware(this)){
+        	postToast("The hardware is available");
+        	mCamera = CameraActivity.getCameraInstance();
+
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+
+        } else {
+        	postToast("The hardware is NOT available");
+        }
 
     }
-	
-    /** Processes the default button pressed method. By default it assumes it is an unknown buttons. */
-	public void onBtnClick(View view) {
-		startActivityAndFinish(BaseActivity.class);
-	}
-	
-	/** Starts a new activity with FLAG_ACTIVITY_CLEAR_TOP and finishes this activity */
-	protected void startActivityAndFinish(Class<?> cls) { 
-		// no point in switching to the same activity or to null
-		if(cls == null || this.getClass() == cls){ return; }
-
-		// start the activity
-        Intent newIntent = new Intent(this, cls);
-		setResult(RESULT_FINISH);
-		startActivity(newIntent);
-		
-		// and finish
-		finish();
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if(resultCode == RESULT_FINISH){
-			setResult(RESULT_FINISH);
-			finish();
-		}	
-	}
-
 	
 	/** Launch a toast message. */
 	protected void postToast(String text){
@@ -64,6 +57,45 @@ public class CameraActivity extends Activity {
 		// show the toast message
 		toastMsg.show();
 	}
+	
+	
+	/** Check if this device has a camera */
+	private boolean checkCameraHardware(Context context) {
+	    if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+	        // this device has a camera
+	        return true;
+	    } else {
+	        // no camera on this device
+	        return false;
+	    }
+	}
+	
+	/** A safe way to get an instance of the Camera object. */
+	public static Camera getCameraInstance(){
+	    Camera c = null;
+	    try {
+	        c = Camera.open(); // attempt to get a Camera instance
+	    }
+	    catch (Exception e){
+	        // Camera is not available (in use or does not exist)
+	    }
+	    return c; // returns null if camera is unavailable
+	}
+
+	
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCamera();              // release the camera immediately on pause event
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
 
 	
 
